993d0a2393e530e69289958ac16efdce49ee610c
NateTran <flashingjetgames@gmail.com>
2021-01-08 09:22:04 -0800

    First Commit
    Added the Project

---
+++ CS1351_1/Assets/Scripts/BallDropScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDropScript : MonoBehaviour
+{
+    public GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Falling");
+        ball.GetComponent<Rigidbody>().useGravity = true;
+    }
+}
---
+++ CS1351_1/Assets/Scripts/Lightswitch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    public Light light;
+    public List<Color> colors = new List<Color>();
+    public int kichard = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        colors.Add(Color.red);
+        colors.Add(Color.green);
+        colors.Add(Color.blue);
+        colors.Add(Color.white);
+        colors.Add(Color.magenta);
+        colors.Add(Color.yellow);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("tab"))
+        {
+            light.color = colors[kichard];//insert color here
+            kichard++;
+            if(kichard == 6)
+            {
+                kichard = 0;
+            }
+        }
+    }
+}
